00bd02695f2524da39a3f4f08236d4ad374d8c50
Werner Koch <wk@gnupg.org>
2003-07-17 08:57:13 +0000

    * src/gpg-error.h: Add HARDWARE, PIN_BLOCKED and USE_CONDITIONS.

--- src/gpg-error.h
+++ src/gpg-error.h
@@ -217,7 +217,10 @@ typedef enum
     GPG_ERR_WRONG_BLOB_TYPE = 127,      /* Keybox BLOB of wrong type.  */
     GPG_ERR_MISSING_VALUE = 128,        /* A required value is missing.  */
 
-    /* 129 to 149 are free to be used.  */
+    GPG_ERR_HARDWARE = 129,             /* e.g. EEPROM error of a smartcard. */
+    GPG_ERR_PIN_BLOCKED = 130,
+    GPG_ERR_USE_CONDITIONS = 131,       /* e.g. used with smartcards. */
+    /* 132 to 149 are free to be used.   */
 
     GPG_ERR_INV_ENGINE = 150,
     GPG_ERR_PUBKEY_NOT_TRUSTED = 151,
