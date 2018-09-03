a2cb8c2451d9df5bded10d767eec263cd2739e0a
NIIBE Yutaka <gniibe@fsij.org>
2018-09-03 13:00:10 +0900

    Not by "make check" but gpg-error-config is determined by the test.

--- src/gpg-error-config-test.sh
+++ src/gpg-error-config-test.sh
@@ -4,31 +4,31 @@ PKG_CONFIG_PATH="."
 
 export PKG_CONFIG_PATH
 
-OUTPUT_OLD=$(./gpg-error-config --version)
+OUTPUT_OLD=$(./gpg-error-config-old --version)
 OUTPUT_NEW=$(./gpg-error-config-new --version)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --libs)
+OUTPUT_OLD=$(./gpg-error-config-old --libs)
 OUTPUT_NEW=$(./gpg-error-config-new --libs)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --cflags)
+OUTPUT_OLD=$(./gpg-error-config-old --cflags)
 OUTPUT_NEW=$(./gpg-error-config-new --cflags)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --mt --libs)
+OUTPUT_OLD=$(./gpg-error-config-old --mt --libs)
 OUTPUT_NEW=$(./gpg-error-config-new --mt --libs)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --mt --cflags)
+OUTPUT_OLD=$(./gpg-error-config-old --mt --cflags)
 OUTPUT_NEW=$(./gpg-error-config-new --mt --cflags)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --cflags --libs)
+OUTPUT_OLD=$(./gpg-error-config-old --cflags --libs)
 OUTPUT_NEW=$(./gpg-error-config-new --cflags --libs)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
-OUTPUT_OLD=$(./gpg-error-config --mt --cflags --libs)
+OUTPUT_OLD=$(./gpg-error-config-old --mt --cflags --libs)
 OUTPUT_NEW=$(./gpg-error-config-new --mt --cflags --libs)
 [ "$OUTPUT_OLD" = "$OUTPUT_NEW" ] || exit 99
 
