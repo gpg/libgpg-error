99ae862a96a569724f49a604ebb7d3f6d2c2d374
NIIBE Yutaka <gniibe@fsij.org>
2021-02-09 11:58:22 +0900

    Support cross-compiling on more platforms.
    * src/gen-lock-obj.sh (ECHO_C, ECHO_N): Portability fix.
    
    --
    
    GnuPG-bug-id: 5289
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- src/gen-lock-obj.sh
+++ src/gen-lock-obj.sh
@@ -38,6 +38,14 @@
 #     AWK=gawk ./gen-lock-obj.sh
 #
 
+if test -n `echo -n`; then
+    ECHO_C='\c'
+    ECHO_N=''
+else
+    ECHO_C=''
+    ECHO_N='-n'
+fi
+
 AWK_VERSION_OUTPUT=$($AWK 'BEGIN { print PROCINFO["version"] }')
 if test -n "$AWK_VERSION_OUTPUT"; then
     # It's GNU awk, which supports PROCINFO.
@@ -84,17 +92,17 @@ EOF
 #     USE_LONG_DOUBLE_FOR_ALIGNMENT
 #
 
-echo -n "#define GPGRT_LOCK_INITIALIZER {$LOCK_ABI_VERSION,{{"
+echo ${ECHO_N} "#define GPGRT_LOCK_INITIALIZER {$LOCK_ABI_VERSION,{{${ECHO_C}"
 
 i=0
 while test "$i" -lt $ac_mtx_size; do
     if test "$i" -ne 0 -a "$(( $i % 8 ))" -eq 0; then
         echo ' \'
-        echo -n "                                    "
+        echo ${ECHO_N} "                                    ${ECHO_C}"
     fi
-    echo -n '0'
+    echo ${ECHO_N} "0${ECHO_C}"
     if test "$i" -lt $(($ac_mtx_size - 1)); then
-        echo -n ','
+        echo ${ECHO_N} ",${ECHO_C}"
     fi
     i=$(( i + 1 ))
 done
